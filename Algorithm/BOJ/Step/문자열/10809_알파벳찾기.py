553534adab749a09d9fc567301fdf296b0eb9640
dbj2000 <dbj2000@naver.com>
2021-09-03 00:09:47 +0900

    #fix : boj 문자열

--- Algorithm/BOJ/Step/문자열/10809_알파벳찾기.py
+++ Algorithm/BOJ/Step/문자열/10809_알파벳찾기.py
@@ -2,11 +2,14 @@ import re
 
 word = input()
 
-alphabet = [chr(c) for c in range(ord('A'),ord('X'))]
+alphabet = [chr(c) for c in range(ord('a'),ord('z')+1)]
+result = []
 
 for w in alphabet:
     match_char = re.search(w, word)
     if match_char is None:
-        print("-1 ")
+        result.append("-1")
     else:
-        print(f"{match_char.start()} ")
+        result.append(str(match_char.start()))
+
+print(' '.join(map(str, result)))
