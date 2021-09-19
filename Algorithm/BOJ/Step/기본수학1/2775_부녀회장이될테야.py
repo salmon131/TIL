f655e244a333cc1104c7954081d10a14d737c5c3
dbj2000 <dbj2000@naver.com>
2021-09-19 16:59:59 +0900

    #fix: 기본수학1_부녀회장

--- Algorithm/BOJ/Step/기본수학1/2775_부녀회장이될테야.py
+++ Algorithm/BOJ/Step/기본수학1/2775_부녀회장이될테야.py
@@ -1,27 +1,41 @@
-# T = int(input())
-# for i in range(T):
-#     k = int(input())
-#     n = int(input())
-#     people = find(k, n)
-#     print(people)
+"""
+예시 답은 출력되었으나 시간초과로 실패한 답
+"""
 
-# def find(k, n):
-#     sum_num = k-1
+def list_generator(n):
+    n_list = []
+    for i in range(1, n+1):
+        n_list.append(i)
+    return n_list
 
-def acc_sum(a):
-    if a<=1:
-        return 1
-    return a + acc_sum(a-1)
+def find(k, n):
+    n_list = list_generator(n)
+    for i in range(k-1, 0, -1):
+        j = 0
+        while True:
+            push_list = list_generator(n_list[j])
+            n_list[j:j+1] = push_list
+            j = j+len(push_list)
+            # print(j)
+            # print(n_list)
+            if j == len(n_list):
+                break
+    return sum(n_list)
 
-# print(acc_sum(T))
+if __name__ == "__main__":
+    T = int(input())
+    for i in range(T):
+        k = int(input())
+        n = int(input())
+        people = find(k, n)
+        print(people)
 
-# k층 n호
-def nested(k, n):
-    sum=0
-    if k<=1:
-        sum += acc_sum(n)
-        return sum
-    return sum + nested(k-1, n)
+# print(find(2, 3))
+
+# li = [1, 2, 3, 4, 5]
+# li[0:1] = [1, 3, 5]
+# print(li)
 
 
-print(nested(2, 2))
+
+
