e5534a44f45a00507359045e823b03672b75e521
jin <jin@jinui-MacBookPro.local>
2022-08-02 15:03:46 +0900

    add: bfs 탐색

--- Algorithm/BOJ/ECT/1260_DFS와BFS.py
+++ Algorithm/BOJ/ECT/1260_DFS와BFS.py
@@ -3,9 +3,32 @@ from collections import defaultdict
 
 N, M, V = map(int, sys.stdin.readline().strip().split())
 
-graph = defaultdict(list)
+graph = defaultdict(set)
 for _ in range(M):
     node_a, node_b = map(int, sys.stdin.readline().strip().split())
-    graph[node_a].append(node_b)
+    graph[node_a].add(node_b)
+    graph[node_b].add(node_a)
 
-print(graph)
+def dfs_recusive(graph, start, visited=[]):
+    visited.append(start)
+    for node in graph[start]:
+        if node not in visited:
+            dfs_recusive(graph, node, visited)
+    return visited
+
+result = map(str, dfs_recusive(graph, V))
+print(' '.join(result))
+
+def bfs(graph, start):
+    tovisit, visited  = [], []
+    tovisit.append(start)
+    while tovisit:
+        node = tovisit[0]
+        del tovisit[0]
+        if node not in visited:
+            visited.append(node)
+            tovisit.extend(graph[node])
+    return visited
+
+result = map(str, bfs(graph, V))
+print(' '.join(result))
