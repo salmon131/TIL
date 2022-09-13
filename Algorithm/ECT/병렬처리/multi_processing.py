a86ed2ddd65dcac76ecb1d8f9dba3ebe307edb9a
jin <jin@jinui-MacBookPro.local>
2022-09-13 23:05:35 +0900

    add: 멀티 프로세싱 테스트

---
+++ Algorithm/ECT/병렬처리/multi_processing.py
@@ -0,0 +1,32 @@
+import time
+from multiprocessing import Process, Queue
+
+def work(id, start, end, result):
+    total = 0
+    for i in range(start, end):
+        total += i
+    result.put(total)
+    return
+
+if __name__ == "__main__":
+    START, END = 0, 100000000
+    result = Queue()
+    start_time = time.time()
+    th1 = Process(target=work, args=(1, START, END//2, result))
+    th2 = Process(target=work, args=(2, END//2, END, result))
+    
+    th1.start()
+    th2.start()
+    th1.join()
+    th2.join()
+
+    result.put('STOP')
+    total = 0
+    while True:
+        tmp = result.get()
+        if tmp == 'STOP':
+            break
+        else:
+            total += tmp
+    print(f"Execute Time: {time.time() - start_time}")
+    print(f"Result: {total}")
