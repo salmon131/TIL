5a52f8dfef39a6015ea90ecf559e1db2e835962b
jin <jin@jinui-MacBookPro.local>
2022-09-21 09:51:33 +0900

    fix: 오타 수정-성공

--- Algorithm/BOJ/DP/2579_계단오르기.py
+++ Algorithm/BOJ/DP/2579_계단오르기.py
@@ -26,10 +26,10 @@ arr = [int(sys.stdin.readline().strip()) for _ in range(N)]
 dp = []
 
 dp.append(arr[0]) # dp[0]
-if N >= 2:
+if N > 1:
     dp.append(max(arr[0] + arr[1], arr[1])) # dp[1]
-if N >= 3:
-    dp.append(max(arr[0] + arr[1], arr[1] + arr[2])) # dp[2]
+if N > 2:
+    dp.append(max(arr[0] + arr[2], arr[1] + arr[2])) # dp[2]
 for i in range(3, N):
     dp.append(max(dp[i-2] + arr[i], dp[i-3] + arr[i-1] + arr[i]))
 
