2e70e576bcb817377a6292b8bc86faa9e5532f49
jin <dbj2000@naver.com>
2023-01-05 23:45:44 +0900

    [0105] 바킹독 0x04강: 요세푸스

---
+++ Algorithm/BaaaaaaaaaaarkingDog/0x04강_연결리스트/1158_요세푸스_문제.py
@@ -0,0 +1,48 @@
+import sys
+input = sys.stdin.readline
+class CList:
+    class Node:
+        def __init__(self,item,link):
+            self.item = item
+            self.next = link
+    
+    def __init__(self):
+        self.last = None
+        self.size = 0
+    
+    def insert(self,item):
+        n = self.Node(item,None)
+        if(self.size == 0):
+            n.next = n
+            self.last = n
+        else:
+            n.next = self.last.next
+            self.last.next = n
+        self.size += 1
+    
+    def delete(self,p):
+        x = p.next
+        if(self.size == 1):
+            self.last = None
+        else:
+            p.next = x.next
+        self.size -= 1
+        return x.item
+ 
+    def print_list(self,N,K):
+        L = []
+        p = self.last
+        for _ in range(N):
+            for j in range(K-1):
+                p = p.next
+            L.append(self.delete(p))
+        return L
+ 
+N,K = map(int,input().split())
+c = CList()
+for i in range(N,0,-1):
+    c.insert(i)
+L = c.print_list(N,K)
+print('<',end='')
+print(', '.join(str(x) for x in L),end='')
+print('>')
