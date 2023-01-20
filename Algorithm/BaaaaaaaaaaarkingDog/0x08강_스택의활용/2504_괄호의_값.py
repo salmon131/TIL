01e0759c77c4b961d5aaa0016e4728822b243e52
jin <dbj2000@naver.com>
2023-01-20 21:36:31 +0900

    [0116] 바킹독 0x08강: 괄호의 값

---
+++ Algorithm/BaaaaaaaaaaarkingDog/0x08강_스택의활용/2504_괄호의_값.py
@@ -0,0 +1,33 @@
+import sys
+
+k = sys.stdin.readline().strip()
+
+stack = []
+answer = 0
+tmp = 1
+
+for i in range(len(k)):
+    if k[i] == '(':
+        stack.append(k[i])
+        tmp *= 2
+    elif k[i] == '[':
+        stack.append(k[i])
+        tmp *= 3
+    elif k[i] == ')':
+        if not stack or stack[-1] == '[':
+            answer = 0
+            break
+        if k[i-1] == '(':
+            answer += tmp
+        stack.pop()
+        tmp //= 2
+    else: # k[i] == ']':
+        if not stack or stack[-1] == '(':
+            answer = 0
+            break
+        if k[i-1] == '[':
+            answer += tmp
+        stack.pop()
+        tmp //= 3
+
+print(answer)
