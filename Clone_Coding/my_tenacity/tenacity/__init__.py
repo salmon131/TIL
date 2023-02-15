7f7d1b685f34f04db505b3002ff7eda515f7fe07
salmon131 <dbj2000@naver.com>
2023-02-16 08:01:45 +0900

    add init

--- Clone_Coding/my_tenacity/tenacity/__init__.py
+++ Clone_Coding/my_tenacity/tenacity/__init__.py
@@ -63,3 +63,158 @@ class RetryAction(BaseAction):
 
     def __init__(self, sleep: t.SupportsFloat) -> None:
         self.sleep = float(sleep)
+
+
+_unset = object()
+
+
+def _first_set(first: t.Union[t.Any, object], second: t.Any) -> t.Any:
+    return second if first is _unset else first
+
+
+class RetryError(Exception):
+    """Encapsulates the last attempt instance right before giving up."""
+
+    def __init__(self, last_attempt: "Future") -> None:
+        self.last_attempt = last_attempt
+        super().__init__(last_attempt)
+
+    def reraise(self) -> t.NoReturn:
+        if self.last_attempt.failed:
+            raise self.last_attempt.result()
+
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__}[{self.last_attempt}]"
+
+
+class AttemptManager:
+    """Manage attempt context."""
+
+    def __init__(self, retry_state: "RetryCallState"):
+        self.retry_state = retry_state
+
+    def __enter__(self) -> None:
+        pass
+
+    def __exit__(
+        self,
+        exc_type: t.Optional[t.Type[BaseException]],
+        exc_value: t.Optional[BaseException],
+        traceback: t.Optional["types.TracebackType"],
+    ) -> t.Optional[bool]:
+        if exc_type is not None and exc_value is not None:
+            self.retry_state.set_exception((exc_type, exc_value, traceback))
+            return True  # Swallow exception.
+        else:
+            # We don't have the result, actually.
+            self.retry_state.set_result(None)
+            return None
+
+
+if sys.version_info[1] >= 9:
+    FutureGenericT = futures.Future[t.Any]
+else:
+    FutureGenericT = futures.Future
+
+
+class Future(FutureGenericT):
+    """Encapsulates a (future or past) attempted call to a target function."""
+
+    def __init__(self, attempt_number: int) -> None:
+        super().__init__()
+        self.attempt_number = attempt_number
+
+    @property
+    def failed(self) -> bool:
+        """Return whether a exception is being held in this future."""
+
+    @classmethod
+    def constuct(
+        cls, attempt_number: int, value: t.Any, has_exception: bool
+    ) -> "Future":
+        """Construct a new Future object."""
+        fut = cls(attempt_number)
+        if has_exception:
+            fut.set_exception(value)
+        else:
+            fut.set_result(value)
+        return fut
+
+
+class BaseRetrying:
+    ...
+
+
+class RetryCallState:
+    """State related to a single call wrapped with Retrying."""
+
+    def __init__(
+        self,
+        retry_object: BaseRetrying,
+        fn: t.Optional[WrappedFn],
+        args: t.Any,
+        kwargs: t.Any,
+    ) -> None:
+        #: Retry call start timestamp
+        self.start_time = time.monotonic()
+        #: Retry manager object
+        self.retry_object = retry_object
+        #: Function wrapped by this retry call
+        self.fn = fn
+        #: Arguments of the function wrapped by this retry call
+        self.args = args
+        #: Keyword arguments of the function wrapped by this retry call
+        self.kwargs = kwargs
+
+        #: The number of the current attempt
+        self.attempt_number: int = 1
+        #: Last outcome (result or exception) produced by the function
+        self.outcome: t.Optional[Future] = None
+        #: Timestamp of the last outcome
+        self.outcome_timestamp: t.Optional[float] = None
+        #: Time spent sleeping in retries
+        self.idle_for: float = 0.0
+        #: Next action as decided by the retry manager
+        self.next_action: t.Optional[RetryAction] = None
+
+    @property
+    def seconds_since_start(self) -> t.Optional[float]:
+        if self.outcome_timestamp is None:
+            return None
+        return self.outcome_timestamp - self.start_time
+
+    def prepare_for_next_attempt(self) -> None:
+        self.outcome = None
+        self.outcome_timestamp = None
+        self.attempt_number += 1
+        self.next_action = None
+
+    def set_result(self, val: t.Any) -> None:
+        ts = time.monotonic()
+        fut = Future(self.attempt_number)
+        fut.set_result(val)
+        self.outcome, self.outcome_timestamp = fut, ts
+
+    def set_exception(
+        self,
+        exc_info: t.Tuple[
+            t.Type[BaseException], BaseException, "types.TracebackType| None"
+        ],
+    ) -> None:
+        ts = time.monotonic()
+        fut = Future(self.attempt_number)
+        fut.set_exception(exc_info[1])
+        self.outcome, self.outcome_timestamp = fut, ts
+
+    def __repr__(self) -> str:
+        if self.outcome is None:
+            result = "none yet"
+        elif self.outcome.failed:
+            exception = self.outcome.exception()
+            result = f"failed ({exception.__class__.__name__} {exception})"
+        else:
+            result = f"retured {self.outcome.result()}"
+
+        slept = float(round(self.idle_for, 2))
+        clsname = self.__class__.__name__
+        return f"<{clsname} {id(self)}: attempt #{self.attempt_number}; slept for {slept}; last result: {result}>"
