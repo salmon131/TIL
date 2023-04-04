5d933d98c63503a35624f35a2a8f41096d77152a
salmon131 <dbj2000@naver.com>
2023-04-04 09:37:12 +0900

    first commit

---
+++ Clone_Coding/my_semantic_kernel/python/semantic_kernel/configuration/backend_type.py
@@ -0,0 +1,5 @@
+import enum
+
+class BackendType(enum.Enum):
+    Unknown = -1
+    OpenAI = 1
---
+++ Clone_Coding/my_semantic_kernel/python/semantic_kernel/configuration/kernel_config.py
@@ -0,0 +1,5 @@
+from typing import Callable, Dict, List, Optional
+
+from sematic_kenel.ai.open_ai.services.open_ai_config import OpenAIConfig
+
+
